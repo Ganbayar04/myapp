53e64f495662d2dda149d1253abfe3b7fca53cf7
Ganbayar04 <ganbayrerdenebat8@gmail.com>
2024-04-13 16:51:12 +0800

    get,update,delete - TuluwluutOrlogo deer nemew

--- server/controller/tuluwluguutOrlogoController.js
+++ server/controller/tuluwluguutOrlogoController.js
@@ -32,3 +32,65 @@ exports.getAllTuluwluguutOrlogo = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.getTuluwluguutOrlogo = async (req, res) => {
+  try {
+    const TuluwluguutOrlogo = await TuluwluguutOrlogo.findById(
+      req.params.id
+    ).populate("tusuw_id");
+    if (!TuluwluguutOrlogo) {
+      return res
+        .status(404)
+        .json({ message: "Уучлаарай! төлөвлөгөөт орлого олдсонгүй." });
+    }
+    res.status(200).json(TuluwluguutOrlogo);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateTuluwluguutOrlogo = async (req, res) => {
+  try {
+    const updatedTuluwluguutOrlogo = await TuluwluguutOrlogo.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!TuluwluguutOrlogo) {
+      return res
+        .status(404)
+        .json({ message: "Уучлаарай! төлөвлөгөөт орлого олдсонгүй." });
+    }
+    res.status(200).json({
+      message: "Төлөвлөгөөт орлогын мэдээлэл амжилттай солигдлоо.",
+      data: {
+        id: updatedTuluwluguutOrlogo._id,
+        name: updatedTuluwluguutOrlogo.name,
+        tailbar: updatedTuluwluguutOrlogo.tailbar,
+        dun: updatedTuluwluguutOrlogo.dun,
+        tusuw_id: updatedTuluwluguutOrlogo.tusuw_id,
+        guitsetgel: updatedTuluwluguutOrlogo.guitsetgel,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.deleteTuluwluguutOrlogo = async (req, res) => {
+  try {
+    const TuluwluguutOrlogo = await TuluwluguutOrlogo.findByIdAndDelete(
+      req.params.id
+    );
+    if (!plannedIncome) {
+      return res
+        .status(404)
+        .json({ message: "Уучлаарай! төлөвлөгөөт орлого олдсонгүй." });
+    }
+    res.status(204).json({ message: "Амжилттай устгалаа." });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
