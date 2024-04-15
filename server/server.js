adf719aa1bb21c0ea6ad9089c1b08502d976e4df
Ganbayar04 <ganbayrerdenebat8@gmail.com>
2024-04-15 17:00:33 +0800

    server.js orlogoTurul path nemew

--- server/server.js
+++ server/server.js
@@ -5,8 +5,12 @@ const userRouter = require("../server/router/userRouter.js");
 const dansRouter = require("../server/router/dansRouter.js");
 const dansTurulRouter = require("../server/router/dansTurulRouter.js");
 const tusuwRouter = require("../server/router/tusuwRouter.js");
+// Төлөвлөгөөт орлого
 const tuluwluguutOrlogoRouter = require("../server/router/tuluwluguutOrlogoRouter.js");
+// төлөвлөгөөт зарлага
 const tuluwluguutZarlagaRouter = require("../server/router/tuluwluguutZarlagaRouter.js");
+// Орлого_төрөл
+const orlogoTurulRouter = require("../server/router/orlogoTurulRouter.js");
 
 require("dotenv").config({ path: "../server/config/.env" });
 require("../server/config/db.js");
@@ -22,6 +26,8 @@ app.use("/api/tusuw", tusuwRouter);
 app.use("/api/tuluw", tuluwluguutOrlogoRouter);
 // Төлөвлөгөөт зарлага
 app.use("/api/tZarlaga", tuluwluguutZarlagaRouter);
+// Орлого_төрөл
+app.use("/api/oTurul", orlogoTurulRouter);
 
 app.listen(9090, "192.168.1.229", () => {
   console.log("port is listening");
