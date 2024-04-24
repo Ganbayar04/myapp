f3397197d07a15959ee9c55b3b12bfe54c8f7d99
Ganbayar04 <ganbayrerdenebat8@gmail.com>
2024-04-25 03:23:09 +0800

    dansEdit uusgew

---
+++ client/screens/dans/dansEdit.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  ScrollView,
+  ActivityIndicator,
+  StyleSheet,
+  Alert,
+  Text,
+} from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { useNavigation } from "@react-navigation/native";
+import DarkMode from "../../styles/darkMode";
+import CustomButton from "../../styles/customButton";
+import API from "../../config";
+import { useUser } from "../../src/contexts/userContext";
+
+const Zasah = () => {};
+
+const styles = StyleSheet.create({});
+
+export default Zasah;
