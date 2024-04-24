c7945a413e7120317e695dd0348a1da28bbbaa6a
Ganbayar04 <ganbayrerdenebat8@gmail.com>
2024-04-25 02:51:48 +0800

    udateStatus nemew

--- server/router/dansRouter.js
+++ server/router/dansRouter.js
@@ -6,6 +6,9 @@ router.post("/", dansController.createDans);
 router.get("/", dansController.getAllDans);
 router.get("/:id", dansController.getDans);
 router.put("/:id", dansController.updateDans);
+// Дансны төлөв өөрчлөх
+router.put('/updateStatus/:id', dansController.updateAccountStatus);
+
 router.delete("/:id", dansController.deleteDans);
 
 module.exports = router;
